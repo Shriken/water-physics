bf9123d51b3ec0b4769c37bd41ddf13747d42ce8
Zane Sterling <sterling.zane@gmail.com>
2014-10-30 16:19:22 -0400

    Add height and flow data to simulation

--- src/Simulation.java
+++ src/Simulation.java
@@ -3,6 +3,8 @@ package WaterPhysics;
 public class Simulation {
 
 	private final int width, height;
+	private double[][][] flows;
+	private double[][] heights;
 
 	public Simulation() {
 		this(20, 20);
@@ -11,6 +13,9 @@ public class Simulation {
 	public Simulation(int width, int height) {
 		this.width = width;
 		this.height = height;
+
+		flows   = new double[height][width][4];
+		heights = new double[height][width];
 	}
 
 	public void update() {
@@ -19,4 +24,12 @@ public class Simulation {
 
 	public int getWidth()  { return width; }
 	public int getHeight() { return height; }
+
+	public double getHeightAt(int x, int y) {
+		return heights[y][x];
+	}
+
+	public double getFlowsAt(int x, int y) {
+		return flows[y][x];
+	}
 }
