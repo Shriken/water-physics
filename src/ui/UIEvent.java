1b4904b418cebb076ac15011832ba58e3c5f9665
Zane Sterling <sterling.zane@gmail.com>
2014-10-31 00:09:57 -0400

    Implement skeleton API for UIEvents

--- src/ui/UIEvent.java
+++ src/ui/UIEvent.java
@@ -2,7 +2,27 @@ package WaterPhysics.ui;
 
 public class UIEvent {
 
-	public UIEvent() {
+	String eventType;
+	int[]    iargs;
+	double[] dargs;
 
+	public UIEvent(String eventType, int[] iargs, double[] dargs) {
+		this.eventType = eventType;
+		this.iargs = iargs;
+		this.dargs = dargs;
+	}
+
+	public UIEvent(String eventType, double[] dargs) {
+		this.eventType = eventType;
+		this.dargs = dargs;
+	}
+
+	public UIEvent(String eventType, int[] iargs) {
+		this.eventType = eventType;
+		this.iargs = iargs;
+	}
+
+	public UIEvent(String eventType) {
+		this.eventType = eventType;
 	}
 }
