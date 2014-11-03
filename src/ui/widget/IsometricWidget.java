413d4d97be0ee9639f8f86760fdaf7c93981464d
Zane Sterling <sterling.zane@gmail.com>
2014-11-03 18:51:47 -0500

    Implement IsometricWidget.isometrizePoint()

--- src/ui/widget/IsometricWidget.java
+++ src/ui/widget/IsometricWidget.java
@@ -8,6 +8,10 @@ import java.awt.event.MouseEvent;
 
 public class IsometricWidget extends UIWidget {
 
+	// the height over width of a rhombus in the iso-grid
+	double aspectRatio;
+	double displayScale;
+
 	public IsometricWidget(int x, int y, int width, int height,
 	                       UI parent) {
 		super(x, y, width, height, parent);
@@ -15,6 +19,19 @@ public class IsometricWidget extends UIWidget {
 
 	public void render(Graphics g) {
 		// TODO write IsometricWidget.render()
+	}
+
+	// convert a point from the sim to an isometric point in the widget
+	private int[] isometrizePoint(int x, int y, int z) {
+		int[] newPoint = new int[2];
+		newPoint[0] = (int) (displayScale * (x + y) / 2);
+		newPoint[1] = (int) (displayScale * (x + y) * aspectRatio / 2);
+
+		return newPoint;
+	}
+
+	private int[] isometrizePoint(int[] coords) {
+		return isometrizePoint(coords[0], coords[1], coords[2]);
 	}
 
 	public void mouseClicked(MouseEvent e) {
