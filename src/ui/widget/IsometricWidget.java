d28a3930c1532a370cba890ea052c270e633b7ef
Zane Sterling <sterling.zane@gmail.com>
2014-11-03 16:15:18 -0500

    Add skeleton for IsometricWidget

---
+++ src/ui/widget/IsometricWidget.java
@@ -0,0 +1,23 @@
+package WaterPhysics.ui.widget;
+
+import WaterPhysics.Simulation;
+import WaterPhysics.ui.UI;
+
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+
+public class IsometricWidget extends UIWidget {
+
+	public IsometricWidget(int x, int y, int width, int height,
+	                       UI parent) {
+		super(x, y, width, height, parent);
+	}
+
+	public void render(Graphics g) {
+		// TODO write IsometricWidget.render()
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		// TODO write IsometricWidget.mouseClicked()
+	}
+}
